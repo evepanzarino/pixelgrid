00d1789f51337645dd77c33796f17cc680b0a19d
Eve Panzarino <109627880+evepanzarino@users.noreply.github.com>
2026-01-07 23:49:51 +0000

    zoom changes

--- client/src/pixelgrid.js
+++ client/src/pixelgrid.js
@@ -20,10 +20,10 @@ export default function PixelGrid() {
 
   // Zoom factor for drawing area based on screen size
   const getZoomFactor = () => {
-    if (size.w <= 768) return 3; // Mobile
+    if (size.w <= 768) return  2.75; // Mobile
     if (size.w <= 1024) return 1.75; // Tablet
-    if (size.w <= 1750) return 1.35;
-    return 1.25; // Desktop
+    if (size.w <= 1750) return 1.25;
+    return 1.1; // Desktop
   };
 
   const zoomFactor = getZoomFactor();
