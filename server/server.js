e44d1d7a1c21134c20843c120122a4ea819976ec
Eve Panzarino <109627880+evepanzarino@users.noreply.github.com>
2026-01-08 14:28:08 +0000

    rebuild

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const path = require('path');
 require('dotenv').config();
 const db = require('./db');
 
@@ -11,6 +12,9 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+// Serve static files from React build
+app.use('/pixelgrid', express.static(path.join(__dirname, '../client/build')));
+
 // Routes
 app.use('/api/users', require('./routes/users'));
 app.use('/api/items', require('./routes/items'));
@@ -18,6 +22,11 @@ app.use('/api/items', require('./routes/items'));
 // Health check endpoint
 app.get('/api/health', (req, res) => {
   res.json({ status: 'Server is running' });
+});
+
+// Serve React app for all other routes under /pixelgrid
+app.get('/pixelgrid/*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
 });
 
 const PORT = process.env.SERVER_PORT || 5000;
